c7655a1b8172ccf95ec02f1eb1f0776d595f5565
DanielAguirre <danter1@gmail.com>
2015-02-26 23:15:44 -0600

    Se escribieron pruebas para spec/todo.js

--- karma.conf.js
+++ karma.conf.js
@@ -17,8 +17,8 @@ module.exports = function(config) {
     files: [
     	'bower_components/angular/angular.js',
 		'bower_components/angular-mocks/angular-mocks.js',
+		//'app/**/*.js',
 		'spec/**/*.js',
-		'app/**/*.js',
 		
     ],
 
--- spec/todo.js
+++ spec/todo.js
@@ -1,12 +1,23 @@
-(function(){
-	describe('Estamos usando una  aplicacion para crear una lista de cosas por hacer',function(){
-		
-		beforeEach(function(){
-			module('todo');
-		});
-		
-		it('Debe definir una lista de objetos',function(){
-			expect(2+2).toEqual(4);
-		});
+describe('Estamos usando una  aplicacion para crear una lista de cosas por hacer',function(){
+	
+	beforeEach(function(){
+		module('todo');
 	});
-}());
+	
+	it('Dede de definir un objeto lista',function(){
+		expect(scope.list).toBeDefined();
+	});
+
+	it('La lista debe de tener almenos un elemento',function() {
+		expect(scope.list.length).toBeGreaterThan(0);
+	});
+
+	it('El primer elemento de la lista debe de ser de tipo String',function() {
+		expect(typeof(scope.list[0])).toBe('string');
+	});
+
+	it('El primer elemento de la lista debe de ser de tipo String',function() {
+		expect(typeof(scope.list[0])).toBe('string');
+	});
+
+});
